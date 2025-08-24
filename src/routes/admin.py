715be9ef686c7604f72ca82726746b286604c6a8
Mind's Eye Admin <admin@mindseye.com>
2025-08-24 15:36:51 -0400

    Add debug alert to slideshow function

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1036,6 +1036,7 @@ dashboard_html = '''
         
         // Slideshow toggle functionality
         async function toggleSlideshow(imageId, currentStatus) {
+            alert('Function called with imageId: ' + imageId + ', currentStatus: ' + currentStatus);
             try {
                 const response = await fetch('/admin/slideshow-toggle', {
                     method: 'POST',
